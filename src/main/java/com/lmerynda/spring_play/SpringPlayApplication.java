7a2da9ae79068feafe07a128b990dfd969ab22a2
lmerynda <lmerynda@gmail.com>
2025-02-20 08:21:58 +0000

    basic spring boot template

---
+++ src/main/java/com/lmerynda/spring_play/SpringPlayApplication.java
@@ -0,0 +1,13 @@
+package com.lmerynda.spring_play;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPlayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lmerynda/spring_play/SpringPlayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmerynda.spring_play;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPlayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
